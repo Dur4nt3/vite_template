022c157c57b7fadf576fdbe0e66dd9436f983cc0
Dur4nt3 <189136258+Dur4nt3@users.noreply.github.com>
2025-07-30 23:51:55 +0300

    Create Vite template
    Created a template for front-end React applications using Vite.

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import './index.css';
+import './assets/stylesheets/reset.css';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+createRoot(document.body).render(<h1>Vite is up and running!</h1>);
